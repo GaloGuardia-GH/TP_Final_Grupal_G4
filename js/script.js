f709b5e276e48f9b0cd3868dc20ca7f3861c34a7
Galo Guardia <galoguardia@gmail.com>
2025-06-13 04:32:01 -0300

    FEAT: Actualizacion de archivo script js con nuevas funciones sobre interaccion con actividades

--- js/script.js
+++ js/script.js
@@ -29,3 +29,59 @@ function submitForm(form) {
 
     return false;
 }
+
+function handleChangeSearchInput(input) {
+    const value = input.value;
+    if (value != '') {
+        let filteredActivities = atracciones.filter((item) => item.nombre.toLowerCase().includes(value.toLowerCase()) || item.descripcion.toLowerCase().includes(value.toLowerCase()));
+
+        const container = document.getElementById('activities-container');
+        container.innerHTML = '';
+        for (const atraccion of filteredActivities) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.classList.add('m-0');
+            card.classList.add('mb-3');
+            card.classList.add('custom-card-animation');
+            card.innerHTML = `
+                <h2>${atraccion.nombre}</h2>
+                <p>${atraccion.descripcion}</p>
+                <p><strong>Tipo:</strong> ${atraccion.tipo}</p>
+                <p><strong>Edad mínima:</strong> ${atraccion.edadMinima} |
+                <strong>Edad máxima:</strong> ${atraccion.edadMaxima}</p>
+                <p><strong>Altura mínima requerida:</strong> ${atraccion.alturaMinima} cm</p>
+            `;
+            container.appendChild(card);
+        }
+    } else {
+        loadAllActivities();
+    }
+}
+
+function loadAllActivities() {
+    const container = document.getElementById('activities-container');
+    if (container) {
+        container.innerHTML = '';
+        for (const atraccion of atracciones) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.classList.add('m-0');
+            card.classList.add('mb-3');
+            card.classList.add('custom-card-animation');
+            card.innerHTML = `
+                <h2>${atraccion.nombre}</h2>
+                <p>${atraccion.descripcion}</p>
+                <p><strong>Tipo:</strong> ${atraccion.tipo}</p>
+                <p><strong>Edad mínima:</strong> ${atraccion.edadMinima} |
+                <strong>Edad máxima:</strong> ${atraccion.edadMaxima}</p>
+                <p><strong>Altura mínima requerida:</strong> ${atraccion.alturaMinima} cm</p>
+            `;
+            container.appendChild(card);
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadAllActivities());
+
+window.submitForm = submitForm;
+window.handleChangeSearchInput = handleChangeSearchInput;
